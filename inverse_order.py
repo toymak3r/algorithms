a74852202ef138027760d5a862b2ee6b3c865272
Edward 'Toy' Facundo <toymak3r@gmail.com>
2019-12-31 13:09:21 -0300

    Update inverse order
    using range instead of for loop.
    
    Signed-off-by: Edward 'Toy' Facundo <toymak3r@gmail.com>

--- inverse_order.py
+++ inverse_order.py
@@ -9,10 +9,22 @@ def inverse(word):
                 i += 1
         return new_word              
 
+def inverse_ver_two(word):
+	n = int(len(word)-1)
+	new_word = ''
+	for i in range(0,n+1):
+		new_word += word[n-i]
+	return new_word	
 
-print("Using Own Rever Function: \n")
+
+print("Using Own Reverse Function: \n")
 start_time = time.time()
 print(inverse("casa"))
+print("--- %s seconds ---" % (time.time() - start_time)+"\n")
+
+print("Using Own Reverse Function Version 2: \n")
+start_time = time.time()
+print(inverse_ver_two("casa"))
 print("--- %s seconds ---" % (time.time() - start_time)+"\n")
 
 print("Using Python's Reverse Function: \n")
